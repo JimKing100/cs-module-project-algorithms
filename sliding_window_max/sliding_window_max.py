177fdef3bc46d586d77c0eaee75feb9a9d91ba1b
JimKing100 <JimKing100@users.noreply.github.com>
2020-07-23 10:30:13 -0700

    algortihms improved

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,17 +2,42 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+# Use a double-ended queue for O(n) solution
+from collections import deque
+
+
+# Use a double-ended queue to store indexes of array elements
 def sliding_window_max(nums, k):
-    max = 0
     n = len(nums)
+    queue = deque()
     arr = []
 
-    for i in range(n - k + 1):
-        max = nums[i]
-        for j in range(1, k):
-            if nums[i + j] > max:
-                max = nums[i + j]
-        arr.append(max)
+    # Process 1st k (window) elements of array
+    for i in range(k):
+        # For each element, remove previous smaller elements
+        while queue and nums[i] > nums[queue[-1]]:
+            queue.pop()
+        queue.append(i)
+
+    # Process remaining windows (nums[k] to nums[n-1])
+    for i in range(k, n):
+        # Store front element, largest element of previous window
+        arr.append(nums[queue[0]])
+
+        # Remove elements outside this window
+        while queue and queue[0] <= (i - k):
+            # Remove the front of the queue
+            queue.popleft()
+
+        # For each element, remove previous smaller elements
+        while queue and nums[i] >= nums[queue[-1]]:
+            queue.pop()
+
+        # Add the current element to the end of the queue
+        queue.append(i)
+    # Store the maximum element of the last window
+    arr.append(nums[queue[0]])
     return arr
 
 
