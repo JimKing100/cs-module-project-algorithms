ca4ef2676f5de3a994eaf94521744107c4a60539
JimKing100 <JimKing100@users.noreply.github.com>
2020-07-23 19:06:10 -0700

    rsp

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,10 +2,22 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  # Your code here
 
-  pass
+def rock_paper_scissors(n):
+    base = [['rock'], ['paper'], ['scissors']]
+    if n <= 0:
+        return [[]]
+    if n == 1:
+        result = base
+        return result
+    else:
+        arr = []
+        for i in range(3**(n-1)):
+            for j in range(3):
+                prev = rock_paper_scissors(n-1)
+                arr.append(prev[i] + base[j])
+        result = arr
+        return result
 
 
 if __name__ == "__main__":
